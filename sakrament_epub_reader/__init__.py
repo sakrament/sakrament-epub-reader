5fb2bc2a13436c3741abd62887ec3864fe03fc3f
Aliaksei Charkes <aliaksei.charkes@mapbox.com>
2021-01-08 09:00:33 +0300

    html2text conversion

--- sakrament_epub_reader/__init__.py
+++ sakrament_epub_reader/__init__.py
@@ -1,8 +1,10 @@
 import logging
 import docopt
 import pathlib
+import html2text
 import ebooklib
 import ebooklib.epub
+import shutil
 
 help = \
     """
@@ -22,19 +24,53 @@ def main(argv):
     args = docopt.docopt(help, argv=argv)
 
     input_epub_file = pathlib.Path(args['<input_epub_file>']).resolve()
+    output_dir = input_epub_file.parent / input_epub_file.stem
 
     logging.info('Program started')
     logging.info('Input epub file: %s', input_epub_file)
+    logging.info('Output directory: %s', output_dir)
 
     if not input_epub_file.exists():
         logging.error("Can't find input EPUB file: %s", input_epub_file)
         raise Exception()
 
+    if output_dir.exists():
+        logging.warning('Output directory exists. Removing it.')
+        shutil.rmtree(output_dir, ignore_errors=True)
+
+    output_dir.mkdir()
+
     book = ebooklib.epub.read_epub(input_epub_file)
 
-    for i in book.get_items():
-        if i.get_type() != ebooklib.ITEM_DOCUMENT:
+    logging.info("Metadata id: %s", book.get_metadata('DC', 'identifier'))
+    logging.info("Metadata title: %s", book.get_metadata('DC', 'title'))
+    logging.info("Metadata language: %s", book.get_metadata('DC', 'language'))
+
+    h2t = html2text.HTML2Text()
+    h2t.ignore_links = True
+    h2t.ignore_images = True
+    h2t.ignore_tables = True
+    h2t.ignore_images = True
+    h2t.images_to_alt = True
+    #h2t.body_width = 0  # don't wrap
+
+    for idx, spine in enumerate(book.spine):
+        id = spine[0]
+        linear = spine[1]
+        formatted_idx = '{:03d}'.format(idx)
+
+        item = book.get_item_with_id(id)
+        if item.get_type() != ebooklib.ITEM_DOCUMENT:
             continue
-        logging.info("Item: %s", i.get_name())
+
+        body = item.get_body_content().decode("utf-8")
+        clean_body = h2t.handle(body)
+
+        logging.info("Item: %s %s", formatted_idx, item.get_name())
+
+        chapter_path = output_dir / f'{formatted_idx}_{pathlib.Path(id).stem}.txt'
+
+        with chapter_path.open('w') as f:
+            f.write(clean_body)
 
     logging.info('OK')
--- sakrament_epub_reader/__main__.py
+++ sakrament_epub_reader/__main__.py
@@ -1,5 +1,4 @@
 import sakrament_epub_reader
 import sys
 
-if __name__ == '__main__':
-    sakrament_epub_reader.main(sys.argv[1:])
+sakrament_epub_reader.main(sys.argv[1:])
