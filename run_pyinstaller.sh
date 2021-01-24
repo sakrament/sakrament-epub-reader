01a473b34ada84c7f2a8d71f94d734b90f28120d
Aliaksei Charkes <aliaksei.charkes@mapbox.com>
2021-01-24 20:14:42 +0300

    Working pyinstaller (macos)

--- run_pyinstaller.sh
+++ run_pyinstaller.sh
@@ -2,10 +2,14 @@
 
 set -x
 
+# always do a clean build
+rm -rf build dist *.spec
+
 pyinstaller \
   --clean \
   --noconfirm \
   --onedir \
+  --name sakrament_epub_reader \
   --console \
   --add-data="sakrament_epub_reader/data:data" \
-  sakrament_epub_reader.py
+  ./sakrament_epub_reader/__main__.py
--- sakrament_epub_reader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-print("Hello world")
-
-# Entrypoint for PyInstaller
-import sakrament_epub_reader
-sakrament_epub_reader.main()
