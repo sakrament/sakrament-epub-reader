89139613ac496d83e24e679847bf3424fbd52c05
valnikgorov <val.nik.egorov@gmail.com>
2021-02-18 13:56:11 +0300

    Изменения 1

--- sakrament_epub_reader/text.py
+++ sakrament_epub_reader/text.py
@@ -58,11 +58,18 @@ class OneToIReplacementProcessor(Processor):
     ошибок системы оптического распознавания текста применяемой у пользователей"""
     def __init__(self):
         super().__init__(
-            pattern=r"(?<=\w)(?<!1)1(?!1)|(?<!1)1(?=\w)(?!1)",
+            pattern=r"1",
             repl="і"
         )
 
 
+class CleanGrafSymbolProcessor(Processor):
+    """ Убрать все символы кроме букв русских и белорусских, цифр и знаков препинания и пробела"""
+    def __init__(self):
+        super().__init__(
+            pattern=r"[^А-ЯЁа-яёА-ЯІЎа-яіў0-9-.,\s]",
+            repl=""
+        )
 class DigitsInTextProcessor(Processor):
     """Чтение цифр по одной, если не сработал ни один из предыдущих вариантов."""
     def __init__(self):
@@ -80,8 +87,9 @@ class DigitsInTextProcessor(Processor):
 processors = [
     DecimalNumberProcessor(),
     IntegerNumberProcessor(),
+    DigitsInTextProcessor(),
     OneToIReplacementProcessor(),
-    DigitsInTextProcessor()
+    CleanGrafSymbolProcessor()
 ]
 
 
